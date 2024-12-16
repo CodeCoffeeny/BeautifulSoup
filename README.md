b33ef654c5f92cde7a315d5d0f1081df74926801
Patrick <cull0026@algonquinlive.com>
2024-12-16 18:51:10 -0500

    started creating the card object in python

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # BeautifulSoup
 Working on projects between semesters
+
+Steps
+Setup documenting/blog enviroment
+Python BeautifulSoup to web scrap STCC for object data
+Create Database
+Produce webpage front with html/javascript to present the database
--- scraper.py
+++ scraper.py
@@ -1,6 +1,23 @@
 import requests
+from bs4 import BeautifulSoup
 
-URL = "https://realpython.github.io/fake-jobs/"
-page = requests.get(URL)
+class Card:
+    affiliate = []    
+    name = ''
+    img = ''
 
-print(page.text)
+card0001 = Card()
+
+DomainURL = "https://www.trekcc.org"
+TargetURL = "/1e/?cardID=0001"
+page = requests.get(DomainURL + TargetURL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+card_tag = soup.find("img",class_="card-big-img")
+card_src = card_tag.get('src')
+
+card0001.img = DomainURL + card_src
+
+''' I think this will be the path to start of card details '''
+
